b3de52f2e2279c713c8c0e265a57d2c4ce8cd9cd
almudena_loquat <almudena.santiago101@alu.ulpgc.es>
2015-04-15 18:26:09 +0100

    DORAMAS ACTUALIZADO CON LOS IMPORT DE PDF BIEN

--- src/main/java/view/GenerarFacturaPDF.java
+++ src/main/java/view/GenerarFacturaPDF.java
@@ -1,7 +1,14 @@
-
 package view;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class GenerarFacturaPDF {
+ 
+    
+    
+    
+    
     
 }
